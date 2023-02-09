cd7b3d6dcf3b02fb9868056c9f67e00201f41e9c
Dario Franzese <dari.franzese@studenti.unina.it>
2023-02-09 19:22:02 +0100

    implementazione gui login

---
+++ Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/LogInActivity.java
@@ -0,0 +1,31 @@
+package com.ingsw.ratatouille23.client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        View view = getWindow().getDecorView();
+        if(hasFocus){
+            view.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        }
+    }
+}
