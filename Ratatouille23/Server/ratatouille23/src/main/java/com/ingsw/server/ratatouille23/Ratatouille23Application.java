2197ce7d1e845a39658249ba8b3a60d71142cf46
Dario Franzese <dari.franzese@studenti.unina.it>
2023-02-10 17:38:40 +0100

    modifiche server + rimozione id_dipendente e id_allergene
    va aggiunta una tabella con attributi nome allergene e elemento per mappare la molti a molti, mi sono dimenticato di farla

--- Ratatouille23/Server/ratatouille23/src/main/java/com/ingsw/server/ratatouille23/Ratatouille23Application.java
+++ Ratatouille23/Server/ratatouille23/src/main/java/com/ingsw/server/ratatouille23/Ratatouille23Application.java
@@ -2,9 +2,16 @@ package com.ingsw.server.ratatouille23;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.modelmapper.ModelMapper;
 
 @SpringBootApplication
 public class Ratatouille23Application {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();	
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Ratatouille23Application.class, args);
