ddb80686282f437cb5326a8fb1c43ae6dbbbaec2
Dario Franzese <dari.franzese@studenti.unina.it>
2023-02-16 18:17:48 +0100

    prima implementazione home
    ti amo

--- Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/View/HomeActivity.java
+++ Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/View/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.ingsw.ratatouille23.client.View;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +14,21 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus){
+        super.onWindowFocusChanged(hasFocus);
+        View view = getWindow().getDecorView();
+        if(hasFocus){
+            view.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
+        }
     }
 }
