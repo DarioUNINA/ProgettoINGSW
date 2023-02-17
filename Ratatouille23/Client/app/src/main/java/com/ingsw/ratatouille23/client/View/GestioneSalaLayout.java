d068e23c03533a824fbe7ae384eae9ecac73c690
Dario Franzese <dari.franzese@studenti.unina.it>
2023-02-17 14:07:13 +0100

    prima implementazione gestione tavoli

---
+++ Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/View/GestioneSalaLayout.java
@@ -0,0 +1,72 @@
+package com.ingsw.ratatouille23.client.View;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.ingsw.ratatouille23.client.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link GestioneSalaLayout#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class GestioneSalaLayout extends Fragment {
+
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    private String mParam1;
+    private String mParam2;
+
+    public GestioneSalaLayout() {
+        // Required empty public constructor
+    }
+
+    public static GestioneSalaLayout newInstance(String param1, String param2) {
+        GestioneSalaLayout fragment = new GestioneSalaLayout();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(R.id.tavoli_layout, new TavoliFragment());
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        View rootView = inflater.inflate(R.layout.fragment_gestione_sala_layout, container, false);
+
+
+
+        return rootView;
+
+
+
+    }
+
+
+}
