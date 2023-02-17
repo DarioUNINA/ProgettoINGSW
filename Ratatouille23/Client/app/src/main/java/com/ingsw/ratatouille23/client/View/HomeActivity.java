9c7e394fec568f426b992f3d2942b5e7f96b5fbb
renato1926 <re.antonelli@studenti.unina.it>
2023-02-17 15:00:59 +0100

    home1

--- Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/View/HomeActivity.java
+++ Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/View/HomeActivity.java
@@ -5,14 +5,26 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.tabs.TabLayout;
 import com.ingsw.ratatouille23.client.R;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TabLayout homeTabLayoutCategory;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        homeTabLayoutCategory = findViewById(R.id.homeTabLayout);
+
+
+        homeTabLayoutCategory.addTab(homeTabLayoutCategory.newTab().setIcon(R.drawable.ic_baseline_people));
+        homeTabLayoutCategory.addTab(homeTabLayoutCategory.newTab().setIcon(R.drawable.table));
+        homeTabLayoutCategory.addTab(homeTabLayoutCategory.newTab().setIcon(R.drawable.ic_baseline_menu_));
+        homeTabLayoutCategory.addTab(homeTabLayoutCategory.newTab().setIcon(R.drawable.chef));
+        homeTabLayoutCategory.setTabGravity(homeTabLayoutCategory.GRAVITY_FILL);
 
     }
 
