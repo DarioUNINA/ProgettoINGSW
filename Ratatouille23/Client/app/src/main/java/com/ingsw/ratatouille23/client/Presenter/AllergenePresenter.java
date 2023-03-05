dce7d555800d05e727c79e850ae3b625b7a575c7
renato1926 <95042058+renato1926@users.noreply.github.com>
2023-03-05 19:15:07 +0100

    spinner finiti

--- Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/Presenter/AllergenePresenter.java
+++ Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/Presenter/AllergenePresenter.java
@@ -1,7 +1,15 @@
 package com.ingsw.ratatouille23.client.Presenter;
 
+import android.widget.ArrayAdapter;
+
+import com.ingsw.ratatouille23.client.Model.Allergene;
+
+import com.ingsw.ratatouille23.client.R;
 import com.ingsw.ratatouille23.client.Service.AllergeneService;
+import com.ingsw.ratatouille23.client.Service.Callback;
 import com.ingsw.ratatouille23.client.View.Dialog.AddElementoMenuDialog;
+
+import java.util.ArrayList;
 
 public class AllergenePresenter {
 
@@ -16,4 +24,27 @@ public class AllergenePresenter {
         this.addElementoMenuDialog = addElementoMenuDialog;
         service = new AllergeneService();
     }
+
+
+    public void getAll(){
+        service.getAll(new Callback() {
+            @Override
+            public void returnResult(Object o) {
+
+                if(o!=null) {
+                    ArrayList<String> listAllergeneString = new ArrayList<String>();
+                    for(Allergene allerg: (ArrayList<Allergene>) o){
+                        listAllergeneString.add(allerg.getNome());
+                    }
+                    addElementoMenuDialog.getAllergeniSpinner().
+                            setAdapter(new ArrayAdapter<String>(addElementoMenuDialog.getContext(),
+                                    R.layout.spinner_item, listAllergeneString));
+                }
+            }
+            @Override
+            public void returnError(Throwable e) {
+            }
+        });
+    }
+
 }
--- Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/View/Dialog/CreazioneUtenteDialog.java
+++ Ratatouille23/Client/app/src/main/java/com/ingsw/ratatouille23/client/View/Dialog/CreazioneUtenteDialog.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Space;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,9 +18,12 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.ingsw.ratatouille23.client.Model.Ruolo;
 import com.ingsw.ratatouille23.client.R;
 import com.ingsw.ratatouille23.client.View.Fragment.FragmentGestionePersonale.PersonaleFragment;
 import com.ingsw.ratatouille23.client.View.Fragment.FragmentGestioneSala.OrdiniFragment;
+
+import java.util.ArrayList;
 
 public class CreazioneUtenteDialog extends AppCompatDialogFragment {
 
@@ -26,6 +31,8 @@ public class CreazioneUtenteDialog extends AppCompatDialogFragment {
 
     private TextView txtNuovoRuolo, txtNuovoNome;
     private Spinner spinnerRuoli;
+
+    ArrayList<String> possibleValues = new ArrayList<String>();
 
 
 
@@ -38,9 +45,29 @@ public class CreazioneUtenteDialog extends AppCompatDialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View v = inflater.inflate(R.layout.dialog_creazione_utente, null);
 
-        spinnerRuoli = v.findViewById(R.id.spinnerRuoli);
+        spinnerRuoli = v.findViewById(R.id.ruoli_spinner);
         txtNuovoNome = v.findViewById(R.id.edtTxtNuovoNomeUtente);
         txtNuovoRuolo = v.findViewById(R.id.edtTxtNuovoRuoloUtente);
+
+        possibleValues.add("Tutti");
+        for (Ruolo r: Ruolo.values()) {
+            possibleValues.add(r.toString());
+        }
+
+
+        spinnerRuoli.setAdapter(new ArrayAdapter<String>(getContext(), R.layout.spinner_item,  possibleValues));
+
+        spinnerRuoli.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                txtNuovoRuolo.setText(spinnerRuoli.getSelectedItem().toString());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
 
 
 
